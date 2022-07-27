9731cbd359155afd4e6d80cc3b89ed2152577cb6
LE Thao <77033487+lthminhthao@users.noreply.github.com>
2022-07-27 17:27:40 +0200

    Delete vectorToMatrix.m

--- vectorToMatrix.m
+++
@@ -1,9 +0,0 @@
-function [ outputMatrix ] = vectorToMatrix( inputVector )
-    vectorLength = length(inputVector);
-
-    outputMatrix = zeros(vectorLength);
-
-    for index = 1:vectorLength
-        outputMatrix(index, index) = inputVector(index);
-    end
-end
